253d7caa63ea34f2a313de7a50ace57c1d13ce52
Yufungus <yufangzengg@gmail.com>
2026-05-21 09:57:10 -0400

    Update Solution.java

--- question-2/Solution.java
+++ question-2/Solution.java
@@ -1,1 +1,16 @@
-
+pubic class bottle{
+  private double cap;
+  private double camt;
+  
+  public bottle(double c){
+    cap=c;    
+    camt=c;
+  }
+  public double updateAmount(double amt){
+    camt-=amt;
+    if(camt<(cap/4)){
+      camt=cap;
+    }
+    return camt;
+  }
+}
