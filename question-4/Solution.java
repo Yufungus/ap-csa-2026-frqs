5a2321cab0086e2851d6e6823530d2ac6a86d284
Yufungus <yufangzengg@gmail.com>
2026-05-28 09:53:41 -0400

    Update Solution.java

--- question-4/Solution.java
+++ question-4/Solution.java
@@ -1,1 +1,10 @@
-
+public int getPointsForRow(int targetRow){
+  int psum=0;
+  String color = board[targetRow][0].getColor;
+  for(int i=0; i<board[targetRow].length;i++){
+    psum+=[targetRow][i].getPoints;
+    if(!board[targetRow][i].getColor().equals(color){
+      return psum*2;
+    }
+    return psum;
+}
