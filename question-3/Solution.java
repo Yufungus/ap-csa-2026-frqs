fe8cd3b106f82d69aa8cf41fed3f568c09fc35f5
Yufungus <yufangzengg@gmail.com>
2026-06-04 00:07:52 -0400

    Update Solution.java

--- question-3/Solution.java
+++ question-3/Solution.java
@@ -1,3 +1,13 @@
 public int moreHistoryThanMathAbsences(){
-  
+  int count=0;
+  for(int i=0; i<historyList.size(); i++){
+    for(int j=0; j<mathList.size(); j++){
+      if(historyList.get(i).getStudentID().equals(mathList.get(j).getStudentID())){
+        if(histRecord.getAbsences()>mathRecord.getAbsences()){
+          count++
+        }
+      }
+    }
+  }
+  return count;
 }
