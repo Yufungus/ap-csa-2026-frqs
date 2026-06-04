23ac691e78cdf0f4fb3ae0ce56928232183e4247
Yufungus <yufangzengg@gmail.com>
2026-06-04 00:17:07 -0400

    Update PartB.java

--- question-1/PartB.java
+++ question-1/PartB.java
@@ -1,3 +1,11 @@
 public String getShortenedName(){
-  
+  String res="";
+  for(int i=0; i<username.length(); i++){
+    if(username.charAt(i)=="-"){
+      res=res.substring(0,res.length()-1);
+    }else{
+      res+=username.charAt(i);
+    }
+  }
+  return res;
 }
